5c06a08a4bfcffba0f9d8dbfbdeea00de17a55b4
kleddy17 <kleddy17@gmail.com>
2021-02-23 08:56:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//ALGO #1
+
+// Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+// Example 1:
+// Input: s = "leetcodeisacommunityforcoders"
+// Output: "ltcdscmmntyfrcdrs"
+
+console.log("hi")
